3bce4d0ffe2526148a1403e072034dccecd05839
Ayato810 <manhattancafe0810520@gmail.com>
2024-03-29 21:05:13 -0500

    modified api baseURL for prod

--- components/Utils/ApiClient.tsx
+++ components/Utils/ApiClient.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  //baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://huawei-cloud.vercel.app/api',
 });
 
 export default api;
