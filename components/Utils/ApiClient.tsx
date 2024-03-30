a82a4b4f34d9c3df41392e952cac83b9dd9cd9bd
Ayato810 <manhattancafe0810520@gmail.com>
2024-03-29 21:44:30 -0500

    changed api back to render

--- components/Utils/ApiClient.tsx
+++ components/Utils/ApiClient.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'http://localhost:8080/api',
-  baseURL: 'https://huawei-cloud.vercel.app/api',
+  baseURL: 'https://huawei-cloud.onrender.com/api',
 });
 
 export default api;
